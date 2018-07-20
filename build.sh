7a9bd109ea3057ef127115afaf0cd85246ed4a5e
n002213f <dev@shoko.co.za>
2018-07-20 08:12:56 +0200

    - force delete dangling images

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 # docker ps -a -q --filter="name=eureka-server"
 mvn clean install
 docker build --build-arg JAR_FILE=target/server-0.0.1-SNAPSHOT.jar --file=Dockerfile --tag=spring-demo/eureka-server:latest .
-docker rmi $(docker images -f 'dangling=true' -q)
+docker rmi $(docker images -f 'dangling=true' -q) -f
 # docker run --publish 8761:8761 --hostname eureka-server --network spring-demo --name eureka-server -i -t spring-demo/eureka-server
